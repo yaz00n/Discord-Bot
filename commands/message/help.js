3b9e9a845ba49c64d068ea2a4200ac8116e33299
Yazoon <101861409+yaz00n@users.noreply.github.com>
2025-09-04 16:34:05 +0300

    Update help.js

--- commands/message/help.js
+++ commands/message/help.js
@@ -57,10 +57,10 @@ module.exports = {
             }
 
             const embed = new EmbedBuilder()
-                .setTitle('📖 Ultimate Music Bot - Command List')
+                .setTitle('📖 Command List')
                 .setColor(0x1DB954)
                 .setDescription(description)
-                .setFooter({ text: 'Developed by GlaceYT | https://glaceyt.com' })
+                .setFooter({ text: 'Developed by Nova Lounge' })
                 .setTimestamp();
 
             await message.reply({ embeds: [embed] });
@@ -71,3 +71,4 @@ module.exports = {
         }
     }
 };
+
