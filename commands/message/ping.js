11f8f1ba939700e1c836f7dbefc74d64257ecd78
Yazoon <101861409+yaz00n@users.noreply.github.com>
2025-09-04 19:35:25 +0300

    Update ping.js

--- commands/message/ping.js
+++ commands/message/ping.js
@@ -35,7 +35,7 @@ module.exports = {
                     `• **Uptime:** ${hours}h ${minutes}m ${seconds}s`
                 )
                 .setTimestamp()
-                .setFooter({ text: 'Ultimate Music Bot • Developed by GlaceYT' });
+                .setFooter({ text: 'Developed by Nova Lounge' });
 
             await message.reply({ embeds: [embed] });
         } catch (error) {
@@ -44,3 +44,4 @@ module.exports = {
         }
     }
 };
+
