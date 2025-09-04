80707d5607ba1fdf949cf92fd94a9567436e25d3
Yazoon <101861409+yaz00n@users.noreply.github.com>
2025-09-04 16:38:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ app.get('/', (req, res) => {
     res.sendFile(imagePath);
 });
 app.listen(port, () => {
-    console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+    console.log(`🔗 Listening to: http://localhost:${port}`);
 });
+
